46f174a868aec450b471cf64c210a66647dee57e
Pattradanai Punvichartkul <saint12302@gmail.com>
2021-07-23 02:10:34 +0700

    add theme change function

--- src/components/models.ts
+++ src/components/models.ts
@@ -1,15 +1,6 @@
-export interface Button {
-  strokeColor: Colors;
-  fontColor: Colors;
-  color: Colors;
-  hoverColor: Colors;
-  path: string;
-}
-
-export enum Colors {
-  WHITE = 'white',
-  BLACK = 'black',
-  LILAC = '#D5B6F1',
-  LAVENDER = '#B87FEC',
-  LIGHT_BLUE = '#F8F8FA',
+export interface Theme {
+  major: string;
+  minor: string;
+  base: string;
+  background: string;
 }
